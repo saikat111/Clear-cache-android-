bd1b673998c9672375f0ede3eb119fec07821328
al rakib sorder <46457169+saikat111@users.noreply.github.com>
2020-12-17 20:37:26 +0600

    Create ClearCache.java

---
+++ ClearCache.java
@@ -0,0 +1,36 @@
+ @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        clearApplicationData();
+    }
+    public void clearApplicationData() {
+        File cache = getCacheDir();
+        File appDir = new File(cache.getParent());
+        if (appDir.exists()) {
+            String[] children = appDir.list();
+            for (String s : children) {
+                if (!s.equals("lib")) {
+                    deleteDir(new File(appDir, s));
+                    Log.i("EEEEEERRRRRROOOOOOORRRR", "**************** File /data/data/APP_PACKAGE/" + s + " DELETED *******************");
+                }
+            }
+        }
+    }
+
+    public static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            int i = 0;
+            while (i < children.length) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+                i++;
+            }
+        }
+
+        assert dir != null;
+        return dir.delete();
+    }
